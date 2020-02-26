470dd90be4159480e3b7b05d0113238ff2dc6f08
Ryota Matsuda <ryota345@outlook.jp>
2020-02-26 14:25:17 +0900

    Finish devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+
+  def after_sign_in_path_for(resource)
+    case resource
+    when Admin
+      admins_items_path
+    when EndUser
+      end_user_path(current_end_user.id)
+    end
+  end
+
+  def after_sign_out_path_for(resource)
+    new_end_user_session_path
+  end
 end
