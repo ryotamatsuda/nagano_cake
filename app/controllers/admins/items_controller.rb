732ff6866831c142df1c26b738177bb741ef4c8b
Ryota Matsuda <ryota345@outlook.jp>
2020-03-01 17:22:19 +0900

    Finish 3 branch

--- app/controllers/admins/items_controller.rb
+++ app/controllers/admins/items_controller.rb
@@ -1,5 +1,21 @@
 class Admins::ItemsController < ApplicationController
   before_action :authenticate_admin!
   def index
+    @all_items = Item.all.includes(:genre)
+  end
+  def new
+    @item = Item.new
+  end
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to admins_items_path
+    else
+      render :new
+    end
+  end
+  private
+  def item_params
+    params.require(:item).permit(:name, :genre_id, :price, :sale_status)
   end
 end
